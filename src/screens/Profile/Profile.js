754da57f1c17677d02f26d2e631046ff334c9ccc
malenabensadon <malenabensadon@MacBook-Pro-de-Malena.local>
2022-11-11 10:32:47 -0300

    estilo profile

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
 import { updatePassword } from "firebase/auth";
-import {auth, db} from '../../firebase/config';
-import { View,
-         Text,
-         TextInput,
-         TouchableOpacity,
-        StyleSheet,
-        FlatList } from 'react-native';
+import { auth, db } from '../../firebase/config';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    FlatList
+} from 'react-native';
 import Post from '../../components/Post/Post';
 
 class Profile extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
             userPosts: [],
             userName: '',
             currentPassword: '',
             newPassword: '',
-            bio:'',
-            userId:'',
+            bio: '',
+            userId: '',
             editSucces: false
         }
     }
 
-   componentDidMount() {
+    componentDidMount() {
         this.getUserPosts.bind(this);
         this.getUserInfo();
         this.getUserPosts();
-   }
+    }
 
-   getUserInfo() {
+    getUserInfo() {
         db.collection('users').where('owner', '==', auth.currentUser.email).onSnapshot(
             docs => {
                 docs.forEach(doc => {
@@ -43,9 +45,9 @@ class Profile extends Component {
                 });
             }
         )
-   }
+    }
 
-   getUserPosts() {
+    getUserPosts() {
         db.collection('posts').where('owner', '==', auth.currentUser.email).orderBy('createdAt', 'desc').onSnapshot(
             docs => {
                 let posts = [];
@@ -60,28 +62,28 @@ class Profile extends Component {
                 })
             }
         )
-   }
+    }
 
-   updateProfileInfo() {
+    updateProfileInfo() {
         db.collection('users')
-        .doc(this.state.userId) //identificar el documento
-        .update({
-            userName: this.state.userName,
-            bio: this.state.bio,
-        })
-        .then(
-            this.getUserInfo()
-        )
-        .catch(e => console.log(e))
-   }
+            .doc(this.state.userId) //identificar el documento
+            .update({
+                userName: this.state.userName,
+                bio: this.state.bio,
+            })
+            .then(
+                this.getUserInfo()
+            )
+            .catch(e => console.log(e))
+    }
 
-   reauthenticate = (user, currentPassword) => {
+    reauthenticate = (user, currentPassword) => {
         auth.signInWithEmailAndPassword(user.email, currentPassword).then((cred) => {
             console.log(cred);
             return user.reauthenticateWithCredential(cred);
-        }) 
+        })
     }
-     
+
     editProfile() {
         if (this.state.password !== '') {
             const user = auth.currentUser;
@@ -97,57 +99,112 @@ class Profile extends Component {
     }
 
 
-    render(){
-        return(
-            <> 
-                <Text>My Profile</Text>
-                <Text>User Name: {this.state.userName}</Text>
-                <Text>Email: {this.state.email}</Text>
-                <Text>Bio: {this.state.bio}</Text>
+    render() {
+        return (
 
 
-                <Text>Cantidad de posteos: {this.state.userPosts.length}</Text>
-                <FlatList 
-                    data={this.state.userPosts}
-                    keyExtractor={ onePost => onePost.id.toString()}
-                    renderItem={({ item }) => <Post postData={item} irAComments={() => {}} refrescarPosts={this.getUserPosts} />}
-                />   
+            <>
+                <View style={styles.container}>
+                    <View style={{ backgroundColor: "black" }}>
+                        <Text style={styles.titulo}></Text>
+                        <Text style={styles.textos}>{this.state.userName}</Text>
+                        <Text style={styles.textos2}>{this.state.email}</Text>
+                        <Text style={styles.textos2}>{this.state.bio}</Text>
+                    </View>
 
-                <Text>Editar Perfil</Text>
-                <TextInput
-                    placeholder='Nombre de usuario'
-                    keyboardType='default'
-                    onChangeText={ text => this.setState({userName: text}) }
-                    value={this.state.userName}
-                />
-                <TextInput
-                    placeholder='Bio'
-                    keyboardType='default'
-                    onChangeText={ text => this.setState({bio: text}) }
-                    value={this.state.bio}
-                />
-                <TextInput
-                    placeholder='ingresar actual contraseña'
-                    keyboardType='default'
-                    secureTextEntry
-                    onChangeText={ text => this.setState({currentPassword: text}) }
-                    value={this.state.currentPassword}
-                />
-                <TextInput
-                    placeholder='nueva contraseña'
-                    keyboardType='default'
-                    secureTextEntry
-                    onChangeText={ text => this.setState({newPassword: text}) }
-                    value={this.state.newPassword}
-                />
-                 <TouchableOpacity onPress={() => this.editProfile()}>
-                    <Text>Editar</Text>
-                </TouchableOpacity>
+                    <Text style={styles.textos2}>Cantidad de posteos: {this.state.userPosts.length}</Text>
+                    <FlatList
+                        data={this.state.userPosts}
+                        keyExtractor={onePost => onePost.id.toString()}
+                        renderItem={({ item }) => <Post postData={item} irAComments={() => { }} refrescarPosts={this.getUserPosts} />}
+                    />
+
+                    <Text style={styles.textos4}>Ingresa lo datos que quieras editar</Text>
+                    <TextInput style={styles.input}
+                        placeholder='Ingresa tu nuevo nombre de usuario'
+                        keyboardType='default'
+                        onChangeText={text => this.setState({ userName: text })}
+                        value={this.state.userName}
+                    />
+                    <TextInput style={styles.input}
+                        placeholder='Ingresa tu nueva biografia'
+                        keyboardType='default'
+                        onChangeText={text => this.setState({ bio: text })}
+                        value={this.state.bio}
+                    />
+                    <TextInput style={styles.input}
+                        placeholder='Ingresa tu actual contraseña'
+                        keyboardType='default'
+                        secureTextEntry
+                        onChangeText={text => this.setState({ currentPassword: text })}
+                        value={this.state.currentPassword}
+                    />
+                    <TextInput style={styles.input}
+                        placeholder='Ingresa tu nueva contraseña'
+                        keyboardType='default'
+                        secureTextEntry
+                        onChangeText={text => this.setState({ newPassword: text })}
+                        value={this.state.newPassword}
+                    />
+                    <TouchableOpacity onPress={() => this.editProfile()}>
+                        <Text style={styles.textos3}>Editar</Text>
+                    </TouchableOpacity>
+
+                </View>
             </>
         )
+
     }
-    
+
 }
+
+const styles = StyleSheet.create({
+    textos: {
+        color: 'white',
+        fontFamily: 'Oswald, sans-serif',
+        fontWeight: 'bold',
+        fontSize: 25,
+        textAlign:'center',
+    },
+    container:{
+        backgroundColor:'black',
+        flex: 1
+    },
+    textos2:{
+        color:'white',
+        fontFamily: 'Oswald, sans-serif',
+        fontWeight: 'bold',
+        fontSize: 10,
+        textAlign:'center',
+        margin: 2,
+    },
+    titulo:{
+        color:'white'
+    },
+    textos3:{
+        color:'white',
+        fontFamily: 'Oswald, sans-serif',
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign:'center',
+    },
+    textos4:{
+        color:'white',
+        fontFamily: 'Oswald, sans-serif',
+        fontWeight: 'bold',
+        fontSize: 15,
+        textAlign:'center',
+    },
+    input:{
+        color:'white',
+        fontFamily: 'Oswald, sans-serif',
+        fontSize: 10,
+        textAlign:'center',
+        margin: 4
+
+    }
+
+})
 
 
 export default Profile;
